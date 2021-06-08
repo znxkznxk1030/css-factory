c6a4fee1508b1ec84952ba811486f9bb2a308827
ysKim <znxkznxk1030@gmail.com>
2021-06-08 12:05:11 +0900

    it makes you sick

--- water-breath/app.js
+++ water-breath/app.js
@@ -8,7 +8,7 @@ class App {
     this.ctx = this.canvas.getContext('2d');
     document.body.appendChild(this.canvas);
 
-    this.wave = new Wave();
+    this.waveGroup = new WaveGroup(8);
 
     window.addEventListener('resize', this.resize.bind(this), false);
     this.resize();
@@ -24,36 +24,62 @@ class App {
     this.canvas.height = this.stageHeight * 2;
 
     this.ctx.scale(2, 2);
-    this.wave.resize(this.stageWidth, this.stageHeight);
+    this.waveGroup.resize(this.stageWidth, this.stageHeight);
   }
 
   animate() {
     this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-    this.wave.draw(this.ctx);
+    this.waveGroup.draw(this.ctx);
     requestAnimationFrame(this.animate.bind(this));
   }
 }
 
+let pointId = 0;
 class Point {
-  constructor(x, y, index) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.originY = y;
 
     this.speed = 0.05;
-    this.weight = index;
+    this.weight = pointId++;
     this.max = Math.random() * 100 + document.body.clientWidth / 50;
     this.min = Math.random() * 100 + document.body.clientWidth / 50;
   }
 
   update() {
     this.weight += this.speed + Math.random() * 0.05;
-    this.y = this.originY + Math.sin(this.weight) * this.max + Math.cos(this.weight) * this.min;
+    this.y =
+      this.originY +
+      Math.sin(this.weight) * this.max +
+      Math.cos(this.weight) * this.min;
   }
 }
-
+let waveId = 0;
+let waveColorSet = [
+  '#F7B0BE',
+  '#FFE8ED',
+  '#E9B0F7',
+  '#807174',
+  '#000000',
+  '#808080',
+];
 class Wave {
   constructor() {
+    this.waveId = waveId++;
+    this.waveColor = waveColorSet[this.waveId % waveColorSet.length];
+  }
+
+  init() {
+    this.numberOfPoint = parseInt(this.stageWidth / 100);
+    this.gapX = this.stageWidth / (this.numberOfPoint - 1);
+    this.points = [];
+
+    for (let i = 0; i < this.numberOfPoint; i++) {
+      this.points.push(
+        new Point(this.gapX * i, this.centerY / 2 + this.waveId * 100)
+      );
+    }
   }
 
   resize(stageWidth, stageHeight) {
@@ -63,23 +89,12 @@ class Wave {
     this.centerX = stageWidth / 2;
     this.centerY = stageHeight / 2;
 
-
     this.init();
-  }
-
-  init() {
-    this.numPoints = parseInt(this.stageWidth / 100);
-    this.gapX = this.stageWidth / (this.numPoints - 1);
-    this.points = []
-
-    for (let i = 0; i < this.numPoints; i++) {
-      this.points.push(new Point(this.gapX * i, this.centerY, i));
-    }
   }
 
   draw(ctx) {
     ctx.beginPath();
-    ctx.fillStyle = '#51c4d3';
+    ctx.fillStyle = this.waveColor;
 
     let prevX = this.points[0].x;
     let prevY = this.points[0].y;
@@ -88,17 +103,17 @@ class Wave {
 
     ctx.moveTo(prevX, prevY);
 
-    for (let i = 0; i < this.numPoints; i++) {
+    for (let i = 0; i < this.numberOfPoint; i++) {
       const point = this.points[i];
       point.update();
 
-      midX = ( prevX + point.x ) / 2
-      midY = ( prevY + point.y ) / 2
+      midX = (prevX + point.x) / 2;
+      midY = (prevY + point.y) / 2;
 
       ctx.quadraticCurveTo(prevX, prevY, midX, midY);
 
-      prevX = point.x
-      prevY = point.y
+      prevX = point.x;
+      prevY = point.y;
       // ctx.arc(point.x, point.y, 20, 0, 2 * Math.PI);
     }
 
@@ -106,5 +121,32 @@ class Wave {
     ctx.lineTo(prevX, this.stageHeight);
     ctx.lineTo(0, this.stageHeight);
     ctx.fill();
+  }
+}
+
+class WaveGroup {
+  constructor(numberOfWave) {
+    this.numberOfWave = numberOfWave ?? 3;
+
+    this.init();
+  }
+
+  init() {
+    this.waves = [];
+    for (let i = 0; i < this.numberOfWave; i++) {
+      this.waves.push(new Wave());
+    }
+  }
+
+  resize(stageWidth, stageHeight) {
+    for (const wave of this.waves) {
+      wave.resize(stageWidth, stageHeight);
+    }
+  }
+
+  draw(context) {
+    for (const wave of this.waves) {
+      wave.draw(context);
+    }
   }
 }
