502f86272f5fade6e06048d83b8c3807a34f7db7
ysKim <znxkznxk1030@gmail.com>
2021-07-01 20:46:51 +0530

    canvas | add app.js

--- canvas/boilerplate/app.js
+++ canvas/boilerplate/app.js
@@ -1,39 +1,39 @@
-let canvas = document.querySelector('canvas');
-let c = canvas.getContext('2d');
+let canvas = document.querySelector("canvas");
+let c = canvas.getContext("2d")
 
 c.width = innerWidth;
 c.height = innerHeight;
 
 let mouse = {
   x: innerWidth / 2,
-  y: innerHeight / 2,
-};
+  y: innerHeight / 2
+}
 
-addEventListener('mousemove', (e) => {
+addEventListener("mousemove", (e) => {
   mouse.x = e.clientX;
   mouse.y = e.clientY;
-});
+})
 
-addEventListener('resize', () => {
+addEventListener("resize", () => {
   c.width = innerWidth;
   c.height = innerHeight;
 
   init();
-});
+})
 
 class Particle {
   constructor(x, y, radius, color) {
     this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+    this.y = y
+    this.radius = radius
+    this.color= color;
   }
 
   draw() {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
+    c.fillStyle = this.color
+    c.fill()
     c.closePath();
   }
 
@@ -44,15 +44,19 @@ class Particle {
 
 const animate = () => {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, c.width, c.height);
-};
+  c.clearRect(0, 0, c.width, c.height)
 
-let object = [];
+}
+
+
+let object = []
 
 const init = () => {
-  object = [];
-  for (let i = 0; i < 400; i++) {}
-};
+  object = []
+  for (let i = 0; i < 400; i++) {
+    
+  }
+}
 
 init();
 animate();
