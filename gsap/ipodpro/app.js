061da48e058e715228785f8295ca6436373b76b8
ysKim <znxkznxk1030@gmail.com>
2021-07-30 21:48:09 +0530

    Gsap | ScrollTrigger screen stop but still scrolling

--- gsap/ipodpro/app.js
+++ gsap/ipodpro/app.js
@@ -1,10 +1,31 @@
 const intro = document.querySelector('.intro');
 const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+gsap.registerPlugin(ScrollTrigger);
+
+let progress = 0;
+
+gsap.to('.container', {
+  scrollTrigger: {
+    trigger: '.container',
+    start: 'top 0%',
+    end: 'bottom 100%',
+    markers: true,
+    pin: intro,
+    onUpdate: (self) => {
+      console.log(self);
+      progress = self.progress;
+    },
+  },
+});
 
 video.currentTime = 0;
 
 
 setInterval(() => {
-  video.currentTime += 0.1;
+  console.log(progress)
+  video.currentTime = video.duration * progress;
   console.log(video.currentTime);
-}, 500);
+}, 100);
+
