19d97414d1af9f619d0d2f6efe026218ed81aee4
ysKim <znxkznxk1030@gmail.com>
2021-08-04 21:41:09 +0530

    Portfolio - 1 | Header #2

--- portfolio/template-1/app.js
+++ portfolio/template-1/app.js
@@ -1,2 +1,8 @@
 let isMenuOpen = false;
 
+const elMenuIcon = document.getElementById('nav-toggle');
+const elNavMenu = document.getElementById('nav-menu');
+
+elMenuIcon.addEventListener('click', () => {
+  elNavMenu.classList.toggle('nav__show');
+});
