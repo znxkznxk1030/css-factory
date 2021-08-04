8777022c582a97e2d422353ab9023da78a42ac22
ysKim <znxkznxk1030@gmail.com>
2021-08-04 21:51:05 +0530

    Portfolio - 1 | Header #4

--- portfolio/template-1/app.js
+++ portfolio/template-1/app.js
@@ -6,3 +6,18 @@ const elNavMenu = document.getElementById('nav-menu');
 elMenuIcon.addEventListener('click', () => {
   elNavMenu.classList.toggle('nav__show');
 });
+
+const listNavLink = document.querySelectorAll('.nav__link');
+
+const removeActiveClass = () => {
+  listNavLink.forEach((elNavLink) => {
+    elNavLink.classList.remove('active');
+  });
+}
+
+listNavLink.forEach((elNavLink) => {
+  elNavLink.addEventListener('click', () => {
+    removeActiveClass();
+    elNavLink.classList.add('active');
+  });
+});
