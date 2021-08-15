732dadc62f08e2831f20e69ceb1b62778e70407a
ysKim <znxkznxk1030@gmail.com>
2021-08-15 19:51:00 +0530

    Canvas | color channel

--- canvas/image-1/app.js
+++ canvas/image-1/app.js
@@ -8,8 +8,8 @@
 const unsplasImg =
   'https://images.unsplash.com/photo-1623435243235-ec41e6c986ab?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYyODk1NzAyMw&ixlib=rb-1.2.1&q=85';
 
-const canvas = document.querySelector("canvas");
-const ctx= canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = 300;
 canvas.height = 300;
@@ -21,14 +21,14 @@ imgObj.onload = function () {
   let nw = imgObj.naturalWidth;
   let nh = imgObj.naturalHeight;
 
-  let aspect = nw/nh;
+  let aspect = nw / nh;
   let h = w / aspect;
   console.log(h);
   canvas.height = h;
 
   ctx.drawImage(imgObj, 0, 0, w, h);
 
-  canvas.addEventListener("click", greyscale);
+  canvas.addEventListener('click', colorChannel);
 
   // ctx.drawImage(imgObj, 500, 1000, 200, 200 / aspect, 0, 0, 300, h);
 };
@@ -59,25 +59,50 @@ const greyscale = function (ev) {
     const ttl = arr[i] + arr[i + 1] + arr[i + 2];
     const avg = ttl / 3;
 
-    arr[i] = avg;       //  red
-    arr[i + 1] = avg;   //  green
-    arr[i + 2] = avg;   //  blue
+    arr[i] = avg; //  red
+    arr[i + 1] = avg; //  green
+    arr[i + 2] = avg; //  blue
   }
   imgData.data.set(arr);
   // imgData.data = arr;
   // imgData.data
   ctx.putImageData(imgData, 0, 0);
+};
 
-}
+const colorChannel = function (ev) {
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  if (origin != null) {
+    console.log('origin: ', origin);
+    imgData.data.set(origin);
+    origin = null;
 
+    ctx.putImageData(imgData, 0, 0);
+    return;
+  }
 
+  origin = imgData.data.slice();
+  let arr = imgData.data.slice();
+
+  for (let i = 0; i < arr.length; i += 4) {
+    const ttl = arr[i] + arr[i + 1] + arr[i + 2];
+    const avg = ttl / 3;
+
+    arr[i] = 0; //  red
+    arr[i + 1] = avg; //  green
+    arr[i + 2] = avg; //  blue
+  }
+  imgData.data.set(arr);
+  // imgData.data = arr;
+  // imgData.data
+  ctx.putImageData(imgData, 0, 0);
+};
 
 /**
  *  utils.js - <https://github.com/christopher4lis/canvas-boilerplate/blob/master/src/js/utils.js>
  *  @function randomIntFromRange Picks a random integer within a range
  *  @function randomColor Picks a random color
  *  @function dispatch Find the distance between two points
- **/ 
+ **/
 
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
